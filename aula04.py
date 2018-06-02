cd7b4c0230b03373113623d95f8c8a82b06c9ff9
jhoyw <jonathan.willians@outlook.com>
2018-06-02 13:50:30 -0300

    Adicionamos a linha de teste de Script

--- aula04.py
+++ aula04.py
@@ -1,5 +1,5 @@
-
 nome = input ('Qual é o Seu nome ?')
 idade = input ('Qual é a Sua idade ?')
 peso = input ('Qual é o seu peso ?')
-print(nome, idade, peso) 
+print(nome, idade, peso)
+print "script realizado com sucesso" 
